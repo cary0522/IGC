3860983da0535f679c6b258a7cecba0a1d035a01
Tracy <cary0522@gmail.com>
2026-06-08 17:38:06 +0800

    安裝 vite-plugin-pwa 及相關設定，並新增必要icon

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
 
 import tailwindcss from '@tailwindcss/vite'
 
@@ -10,6 +11,34 @@ export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'IGC',
+        short_name: 'IGC',
+        description: 'Instagram 照片組圖與影片拼貼工具 | 免費、快速、支援多比例',
+        start_url: '/IGC/',
+        scope: '/IGC/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/IGC/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/IGC/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}']
+      }
+    })
   ],
   resolve: {
     alias: {
